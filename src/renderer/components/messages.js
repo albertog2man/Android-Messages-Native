495a75289cbd16c8f8cdbf1967fc3f6efaa54e9b
albertog2man <albertomgalvez@gmail.com>
2018-08-19 02:19:25 -0400

    Initial commit

---
+++ src/renderer/components/messages.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Messages extends Component {
+    constructor(...args) {
+        super(...args);
+     }
+
+     render(){
+         return(<h1>Test</h1>)
+     }
+}
